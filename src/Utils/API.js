bbb34135d6ec778bc6d7a22f9347b98a67ebd6ea
Raj Seth <sethraj03@gmail.com>
2024-03-17 22:44:25 +0530

    Smart Home 1.2

--- src/Utils/API.js
+++ src/Utils/API.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 
 const BASE_URL = "https://kodessphere-api.vercel.app/devices/";
-const TEST_URL="https://kodessphere-api-test.vercel.app/devices/"
+// const TEST_URL="https://kodessphere-api-test.vercel.app/devices/"
 const TEAM_ID = "IhceXG2";
 
 export const sendDeviceControlRequest = async (device, value) => {
   try {
-    const response = await axios.post(`${TEST_URL}`, {
+    const response = await axios.post(`${BASE_URL}`, {
       teamid: TEAM_ID,
       device: device,
       value: value,
     });
-
-    console.log("Response:", response.data);
     return response.data;
   } catch (error) {
     console.error("Error:", error);
@@ -22,7 +20,7 @@ export const sendDeviceControlRequest = async (device, value) => {
 
 export const getDeviceStatus = async () => {
   try {
-    const response = await axios.get(`${TEST_URL}${TEAM_ID}`);
+    const response = await axios.get(`${BASE_URL}${TEAM_ID}`);
     return response.data;
   } catch (error) {
     console.error("Error:", error);
